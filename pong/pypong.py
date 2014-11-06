9340d6d2335e301414955129030dbdef2c475f8f
Jeremy Kenyon <lanthos@gmail.com>
2014-11-05 20:50:56 -0800

    Resized the ball, resized the paddle thickness, trying to figure out how to make the ball redirect depending on where it hits the paddle.

--- pong/pypong.py
+++ pong/pypong.py
@@ -9,7 +9,7 @@ fps = 200
 WINDOWWIDTH = 700
 WINDOWHEIGHT = 600
 LINETHICKNESS = 10
-PADDLESIZE = 40
+PADDLESIZE = 35
 PADDLEOFFSET = 50
 XBALLSPEED = 3
 YBALLSPEED = 3
@@ -35,10 +35,10 @@ def drawArena():
 
 # Play the sound based upon speed
 def pong_sound():
-    if 3 < NUMHITS < 5:
+    if 3 < YBALLSPEED < 5:
         faster_sound.play()
         #fastest_sound.play()
-    elif NUMHITS > 6:
+    elif YBALLSPEED > 6:
         #faster_sound.play()
         fastest_sound.play()
     else:
@@ -92,14 +92,28 @@ def checkEdgeCollision(ball, ballDirX, ballDirY):
 
 # Paddle collision
 def checkHitBall(ball, paddle1, paddle2, ballDirX):
-    global YBALLSPEED, XBALLSPEED, NUMHITS
+    global YBALLSPEED, XBALLSPEED, NUMHITS, ballDirY
     #if ballDirX == -1 and paddle1.right == ball.left and paddle1.top <= ball.bottom and paddle1.bottom >= ball.top:
     if ballDirX == -1 and paddle1.colliderect(ball):
+        print("general hit: paddle1.bottom = %s, ball.top = %s, ball.bottom = %s" %
+              (paddle1.bottom, ball.top, ball.bottom))
         NUMHITS += 1
         pong_sound()
-        # if ball.bottom <= paddle1.top + 15 or paddle1.bottom - 15 <= ball.top:
-        #     YBALLSPEED = 2
-        #     XBALLSPEED = 2
+        if ball.top >= paddle1.bottom - 7:
+            YBALLSPEED = 5
+            ballDirY = 1
+        elif ball.top >= paddle1.bottom - 14 and ball.bottom <= paddle1.bottom - 7:
+            YBALLSPEED = 4
+            ballDirY = 1
+        elif ball.top >= paddle1.bottom - 21 and ball.bottom <= paddle1.bottom - 14:
+            YBALLSPEED = 0
+            ballDirY = 0
+        elif ball.top >= paddle1.bottom - 28 and ball.bottom <= paddle1.bottom - 21:
+            YBALLSPEED = 4
+            ballDirY = -1
+        elif ball.top >= paddle1.bottom - 35 and ball.bottom <= paddle1.bottom - 28:
+            YBALLSPEED = 5
+            ballDirY = -1
         # else:
         #     YBALLSPEED = 1
         #     XBALLSPEED = 1
@@ -108,12 +122,23 @@ def checkHitBall(ball, paddle1, paddle2, ballDirX):
     elif ballDirX == 1 and paddle2.colliderect(ball):
         NUMHITS += 1
         pong_sound()
-        # if ball.bottom <= paddle2.top + 15 or paddle2.bottom - 15 <= ball.top:
-        #     YBALLSPEED = 2
-        #     XBALLSPEED = 2
-        # else:
-        #     YBALLSPEED = 1
-        #     XBALLSPEED = 1
+        print("general hit: paddle1.bottom = %s, ball.top = %s, ball.bottom = %s" %
+              (paddle2.bottom, ball.top, ball.bottom))
+        if ball.top >= paddle2.bottom - 7:
+            YBALLSPEED = 5
+            ballDirY = 1
+        elif ball.top >= paddle2.bottom - 14 and ball.bottom <= paddle2.bottom - 7:
+            YBALLSPEED = 4
+            ballDirY = 1
+        elif ball.top >= paddle2.bottom - 21 and ball.bottom <= paddle2.bottom - 14:
+            YBALLSPEED = 0
+            ballDirY = 0
+        elif ball.top >= paddle2.bottom - 28 and ball.bottom <= paddle2.bottom - 21:
+            YBALLSPEED = 4
+            ballDirY = -1
+        elif ball.top >= paddle2.bottom - 35 and ball.bottom <= paddle2.bottom - 28:
+            YBALLSPEED = 5
+            ballDirY = -1
         return -1
     else:
         return 1
@@ -152,9 +177,9 @@ def artificialIntelligence(ball, ballDirX, paddle2):
 
 
 def displayScore(score):
-    resultSurf = BASICFONT.render('NUMHITS = %s' %NUMHITS, True, WHITE)
+    resultSurf = BASICFONT.render('HITS = %s, YSPEED = %s, YDIR = %s' % (NUMHITS, YBALLSPEED, ballDirY), True, WHITE)
     resultRect = resultSurf.get_rect()
-    resultRect.topleft = (WINDOWWIDTH - 150, 25)
+    resultRect.topleft = (WINDOWWIDTH / 2, 25)
     DISPLAYSURF.blit(resultSurf, resultRect)
 
 
@@ -165,6 +190,7 @@ def main():
     global DISPLAYSURF
     global BASICFONT, BASICFONTSIZE
     global regular_sound, faster_sound, fastest_sound, wall_sound, miss_sound
+    global ballDirY, ballDirX
     BASICFONTSIZE = 20
     BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
     FPSCLOCK = pygame.time.Clock()
@@ -193,9 +219,9 @@ def main():
     ballDirY = - 1  # -1 = Up and 1 = Down
 
     # Create rects for paddles and ball
-    paddle1 = pygame.Rect(PADDLEOFFSET, playerOnePosition, LINETHICKNESS, PADDLESIZE)
-    paddle2 = pygame.Rect(WINDOWWIDTH - PADDLEOFFSET - LINETHICKNESS, playerTwoPosition, LINETHICKNESS, PADDLESIZE)
-    ball = pygame.Rect(ballX, ballY, LINETHICKNESS, LINETHICKNESS)
+    paddle1 = pygame.Rect(PADDLEOFFSET, playerOnePosition, 6, PADDLESIZE)
+    paddle2 = pygame.Rect(WINDOWWIDTH - PADDLEOFFSET - LINETHICKNESS, playerTwoPosition, 6, PADDLESIZE)
+    ball = pygame.Rect(ballX, ballY, 6, 6)
 
     # Draws the starting position of the arena
     drawArena()
@@ -216,6 +242,8 @@ def main():
             elif event.type == KEYDOWN:
                 if event.key == K_SPACE:
                     NUMHITS += 1
+                if event.key == K_q:
+                    pygame.quit()
 
         drawArena()
         drawPaddle(paddle1)
@@ -225,7 +253,7 @@ def main():
         ball = moveBall(ball, ballDirX, ballDirY)
         ballDirX, ballDirY = checkEdgeCollision(ball, ballDirX, ballDirY)
         score = checkPointScored(paddle1, ball, score, ballDirX)
-        ballDirX = ballDirX * checkHitBall(ball, paddle1, paddle2, ballDirX)
+        ballDirX *= checkHitBall(ball, paddle1, paddle2, ballDirX)
 
         paddle2 = artificialIntelligence(ball, ballDirX, paddle2)
 
