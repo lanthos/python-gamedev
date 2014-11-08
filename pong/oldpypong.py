0b68be9e057a56ca442fce34931e982eb47522d7
Jeremy Kenyon <lanthos@gmail.com>
2014-11-07 18:54:03 -0800

    Added the font and the scores are now on both sides of the screen.  Combined balldir and ballspeed into a single variable.  Using percentages of the paddle size to determine angle of reflect as well as speed.
    Need to add no person playing mode, update so that there is a pause between scoring and the ball going into play again, updating the game to end at a certain score, and having the ball serve to the loser of the last round.  Misc tuning will be needed as well.

--- pong/oldpypong.py
+++ pong/oldpypong.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from pygame.locals import *
 
+
 # Set FPS to allow for changing game speed
 fps = 200
 
@@ -10,7 +11,7 @@ WINDOWWIDTH = 700
 WINDOWHEIGHT = 600
 LINETHICKNESS = 10
 PADDLESIZE = 35
-PADDLEOFFSET = 60
+PADDLEOFFSET = 50
 XBALLSPEED = 3
 YBALLSPEED = 2
 NUMHITS = 0
@@ -59,89 +60,99 @@ def drawPaddle(paddle):
 
 
 # Draw the ball
-def drawBall(ball, color):
-    pygame.draw.rect(DISPLAYSURF, color, ball)
+def drawBall(ball):
+    pygame.draw.rect(DISPLAYSURF, WHITE, ball)
 
 
 # Move the ball
-def moveBall(ball, XBALLSPEED, YBALLSPEED):
-    ball.x += XBALLSPEED  # ballspeed.speed
-    ball.y += YBALLSPEED  # ballspeed.speed
+def moveBall(ball, ballDirX, ballDirY):
+    ball.x += ballDirX * XBALLSPEED  # ballspeed.speed
+    ball.y += ballDirY * YBALLSPEED  # ballspeed.speed
     return ball
 
 
 def tension():
     global NUMHITS, YBALLSPEED, XBALLSPEED
     if NUMHITS > 6:
-        if XBALLSPEED > 0:
-            XBALLSPEED = 5
-        else:
-            XBALLSPEED = -5
-    if 3 < NUMHITS < 6:
-        if XBALLSPEED > 0:
-            XBALLSPEED = 4
-        else:
-            XBALLSPEED = -4
+        XBALLSPEED = 5
+    if 3 < NUMHITS < 5:
+        XBALLSPEED = 4
 
 
 #Checks for a collision with a wall, and 'bounces' ball off it.
 #Returns new direction
-def checkEdgeCollision(ball, XBALLSPEED, YBALLSPEED):
+def checkEdgeCollision(ball, ballDirX, ballDirY):
     if ball.top <= 0 or ball.bottom >= WINDOWHEIGHT:
-        YBALLSPEED *= -1
+        ballDirY *= -1
         wall_sound.play()
-    # if ball.left <= 0 or ball.right >= WINDOWWIDTH:
-    #     XBALLSPEED *= -1
-    #     wall_sound.play()
-    return XBALLSPEED, YBALLSPEED
+    if ball.left <= 0 or ball.right >= WINDOWWIDTH:
+        ballDirX *= -1
+        wall_sound.play()
+    return ballDirX, ballDirY
 
 
 # Paddle collision
-def checkHitBall(ball, paddle1, paddle2, XBALLSPEED):
-    global YBALLSPEED, NUMHITS, ballDirY
-    if XBALLSPEED < 0 and paddle1.colliderect(ball):
+def checkHitBall(ball, paddle1, paddle2, ballDirX):
+    global YBALLSPEED, XBALLSPEED, NUMHITS, ballDirY
+    #if ballDirX == -1 and paddle1.right == ball.left and paddle1.top <= ball.bottom and paddle1.bottom >= ball.top:
+    if ballDirX == -1 and paddle1.colliderect(ball):
+        NUMHITS += 1
+        pong_sound()
+        # Potential future collision code
+        # hit_location = 5 * ((ball.centery - paddle1.centery) / (PADDLESIZE / 2))
+        # if hit_location >= 0:
+        #     YBALLSPEED = hit_location
+        #     ballDirY = 1
+        # else:
+        #     YBALLSPEED = hit_location * -1
+        #     ballDirY = -1
         print("general hit: paddle1.bottom = %s, ball.top = %s, ball.bottom = %s" %
               (paddle1.bottom, ball.top, ball.bottom))
-        NUMHITS += 1
-        pong_sound()
-        YBALLSPEED = 5 * ((ball.centery - paddle1.centery) / (PADDLESIZE / 2))
-        #if ball.top <= paddle1.bottom - 35:
-        #     YBALLSPEED = -5
-        #     # ballDirY = -1
-        # elif ball.top <= paddle1.bottom - 25:
-        #     YBALLSPEED = -4
-        #     # ballDirY = -1
-        # elif ball.top <= paddle1.bottom - 21:
-        #     YBALLSPEED = 0
-        #     #ballDirY = 0
-        # elif ball.top >= paddle1.bottom - 7:
-        #     YBALLSPEED = 4
-        #     # ballDirY = 1
-        # elif ball.top >= paddle1.bottom - 19:
-        #     YBALLSPEED = 5
-        #     #ballDirY = 1
+        if ball.top <= paddle1.bottom - 35:
+            YBALLSPEED = 5
+            ballDirY = -1
+        elif ball.top <= paddle1.bottom - 26:
+            YBALLSPEED = 4
+            ballDirY = -1
+        elif ball.top <= paddle1.bottom - 21:
+            YBALLSPEED = 0
+            ballDirY = 0
+        elif ball.top >= paddle1.bottom - 9:
+            YBALLSPEED = 4
+            ballDirY = 1
+        elif ball.top >= paddle1.bottom - 17:
+            YBALLSPEED = 5
+            ballDirY = 1
         return -1
-    elif XBALLSPEED > 0 and paddle2.colliderect(ball):
+    #elif ballDirX == 1 and paddle2.left == ball.right and paddle2.top <= ball.top and paddle2.bottom >= ball.bottom:
+    elif ballDirX == 1 and paddle2.colliderect(ball):
         NUMHITS += 1
         pong_sound()
-        YBALLSPEED = 5 * ((ball.centery - paddle2.centery) / (PADDLESIZE / 2))
-        print("general hit: paddle1.bottom = %s, ball.top = %s, ball.bottom = %s" %
-              (paddle2.bottom, ball.top, ball.bottom))
-        # if ball.top <= paddle2.bottom - 35:
-        #     YBALLSPEED = -5
-        #     # ballDirY = -1
-        # elif ball.top <= paddle2.bottom - 25:
-        #     YBALLSPEED = -4
-        #     # ballDirY = -1
-        # elif ball.top <= paddle2.bottom - 21:
-        #     YBALLSPEED = 0
-        #     # ballDirY = 0
-        # elif ball.top >= paddle2.bottom - 7:
-        #     YBALLSPEED = 4
-        #     # ballDirY = 1
-        # elif ball.top >= paddle2.bottom - 19:
-        #     YBALLSPEED = 5
-        #     # ballDirY = 1
+        # Potential future collision code
+        # if hit_location >= 0:
+        #     YBALLSPEED = hit_location
+        #     ballDirY = 1
+        # else:
+        #     YBALLSPEED = hit_location * -1
+        #     ballDirY = -1
+        hit_location = 5 * ((ball.centery - paddle2.centery) / (PADDLESIZE / 2))
+        print("general hit: paddle2.bottom = %s, ball.top = %s, ball.bottom = %s, HL = %s" %
+              (paddle2.bottom, ball.top, ball.bottom, hit_location))
+        if ball.top <= paddle2.bottom - 35:
+            YBALLSPEED = 5
+            ballDirY = -1
+        elif ball.top <= paddle2.bottom - 26:
+            YBALLSPEED = 4
+            ballDirY = -1
+        elif ball.top <= paddle2.bottom - 21:
+            YBALLSPEED = 0
+            ballDirY = 0
+        elif ball.top >= paddle2.bottom - 9:
+            YBALLSPEED = 4
+            ballDirY = 1
+        elif ball.top >= paddle2.bottom - 17:
+            YBALLSPEED = 5
+            ballDirY = 1
         return -1
     else:
         return 1
@@ -159,11 +170,10 @@ def checkPointScored(p1_score, p2_score, ballX, ballY, XBALLSPEED):
         miss_sound.play()
         ball = pygame.Rect(ballX, ballY, 6, 6)
     return p1_score, p2_score
-
     # if ball.left == LINETHICKNESS:
     #     # resets the points if the ball hits the left wall
     #     return 0
-    # elif XBALLSPEED < 0 and paddle1.right == ball.left and paddle1.top <= ball.bottom and paddle1.bottom >= ball.top:
+    # elif ballDirX == -1 and paddle1.right == ball.left and paddle1.top <= ball.bottom and paddle1.bottom >= ball.top:
     #     score += 1
     #     return score
     # elif ball.right == WINDOWWIDTH - LINETHICKNESS:
@@ -174,15 +184,15 @@ def checkPointScored(p1_score, p2_score, ballX, ballY, XBALLSPEED):
 
 
 # artificialIntelligence for the computer
-def artificialIntelligence(ball, XBALLSPEED, paddle2):
+def artificialIntelligence(ball, ballDirX, paddle2):
     # If ball is moving away from paddle, center it
-    if XBALLSPEED < 0:
+    if ballDirX == -1:
         if paddle2.centery < (WINDOWHEIGHT / 2):
             paddle2.y += 2
         elif paddle2.centery > (WINDOWHEIGHT / 2):
             paddle2.y -= 2
     # If ball is moving towards paddle, track it
-    elif XBALLSPEED > 0:
+    elif ballDirX == 1:
         if paddle2.centery < ball.centery:
             if YBALLSPEED == 0:
                 paddle2.y += 2
@@ -218,8 +228,8 @@ def main():
     global DISPLAYSURF
     global BASICFONT, BASICFONTSIZE
     global regular_sound, faster_sound, fastest_sound, wall_sound, miss_sound
-    global ball
-    BASICFONTSIZE = 20
+    global ballDirY, ballDirX, ball
+    BASICFONTSIZE = 70
     BASICFONT = pygame.font.Font('visitor1.ttf', BASICFONTSIZE)
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
@@ -231,6 +241,8 @@ def main():
     fastest_sound = pygame.mixer.Sound("fastest.wav")
     wall_sound = pygame.mixer.Sound("wallhit.wav")
     miss_sound = pygame.mixer.Sound("miss.wav")
+    
+
 
     # Initiate variable and set starting positions
     # any future changes made within rectangles
@@ -241,6 +253,10 @@ def main():
     p1_score = 0
     p2_score = 0
 
+    # Keeps track of ball direction
+    ballDirX = - 1  # -1 = Left 1 = Right
+    ballDirY = - 1  # -1 = Up and 1 = Down
+
     # Create rects for paddles and ball
     paddle1 = pygame.Rect(PADDLEOFFSET, playerOnePosition, 6, PADDLESIZE)
     paddle2 = pygame.Rect(WINDOWWIDTH - PADDLEOFFSET - LINETHICKNESS, playerTwoPosition, 6, PADDLESIZE)
@@ -250,11 +266,11 @@ def main():
     drawArena()
     drawPaddle(paddle1)
     drawPaddle(paddle2)
-    drawBall(ball, WHITE)
+    drawBall(ball)
 
     pygame.mouse.set_visible(0)  # hide the cursor
     while True:
-        global NUMHITS, XBALLSPEED, YBALLSPEED
+        global NUMHITS
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -271,14 +287,14 @@ def main():
         drawArena()
         drawPaddle(paddle1)
         drawPaddle(paddle2)
-        drawBall(ball, WHITE)
+        drawBall(ball)
 
-        ball = moveBall(ball, XBALLSPEED, YBALLSPEED)
-        XBALLSPEED, YBALLSPEED = checkEdgeCollision(ball, XBALLSPEED, YBALLSPEED)
+        ball = moveBall(ball, ballDirX, ballDirY)
+        ballDirX, ballDirY = checkEdgeCollision(ball, ballDirX, ballDirY)
         p1_score, p2_score = checkPointScored(p1_score, p2_score, ballX, ballY, XBALLSPEED)
-        XBALLSPEED *= checkHitBall(ball, paddle1, paddle2, XBALLSPEED)
+        ballDirX *= checkHitBall(ball, paddle1, paddle2, ballDirX)
 
-        paddle2 = artificialIntelligence(ball, XBALLSPEED, paddle2)
+        paddle2 = artificialIntelligence(ball, ballDirX, paddle2)
 
         displayScore(p1_score, p2_score, ball)
 
