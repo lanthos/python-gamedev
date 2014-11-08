371d390b553beee1be4b411366b86957740b51a7
Jeremy Kenyon <lanthos@gmail.com>
2014-11-08 15:41:49 -0800

    Updated AI so that ball y can be 0 and player 2 will still try and hit the ball.  Added spectator mode.  Added Game Over.

--- pong/pypong.py
+++ pong/pypong.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import random
+import time
 from pygame.locals import *
 
 # Set FPS to allow for changing game speed
@@ -11,7 +13,7 @@ WINDOWHEIGHT = 600
 LINETHICKNESS = 10
 PADDLESIZE = 35
 PADDLEOFFSET = 60
-XBALLSPEED = -3
+XBALLSPEED = -4
 YBALLSPEED = 2
 NUMHITS = 0
 
@@ -67,14 +69,14 @@ def tension():
     global NUMHITS, YBALLSPEED, XBALLSPEED
     if NUMHITS > 6:
         if XBALLSPEED > 0:
+            XBALLSPEED = 8
+        else:
+            XBALLSPEED = -8
+    if 3 < NUMHITS < 6:
+        if XBALLSPEED > 0:
             XBALLSPEED = 6
         else:
             XBALLSPEED = -6
-    if 3 < NUMHITS < 6:
-        if XBALLSPEED > 0:
-            XBALLSPEED = 4
-        else:
-            XBALLSPEED = -4
 
 
 #Checks for a collision with a wall, and 'bounces' ball off it.
@@ -94,6 +96,9 @@ def checkHitBall(ball, paddle1, paddle2):
         pong_sound()
         YBALLSPEED = 5 * ((ball.centery - paddle1.centery) / (PADDLESIZE / 2))
         # print("numhits = %s, xspeed = %s" % (NUMHITS, XBALLSPEED))
+        pressed = pygame.key.get_pressed()
+        if pressed[K_SPACE]:
+            NUMHITS = 5
         return -1
     elif XBALLSPEED > 0 and paddle2.colliderect(ball):
         NUMHITS += 1
@@ -107,20 +112,39 @@ def checkHitBall(ball, paddle1, paddle2):
 
 # Check to see if a score was made
 def checkPointScored(p1_score, p2_score, ballX, ballY):
-    global ball, NUMHITS, XBALLSPEED
+    global ball, NUMHITS, XBALLSPEED, YBALLSPEED
     if ball.left <= 25:
         p2_score += 1
         miss_sound.play()
         ball = pygame.Rect(ballX, ballY, 6, 6)
         NUMHITS = 0
         XBALLSPEED = -3
+        random.seed()
+        YBALLSPEED = random.randint(-4, -1)
     if ball.right >= WINDOWWIDTH - 25:
         p1_score += 1
         miss_sound.play()
         ball = pygame.Rect(ballX, ballY, 6, 6)
         NUMHITS = 0
         XBALLSPEED = 3
+        random.seed()
+        YBALLSPEED = random.randint(1, 4)
     return p1_score, p2_score
+
+
+def game_over(p1_score, p2_score):
+    global paused, gameover_font
+    if p1_score >= 15 or p2_score >= 15:
+        gameover = gameover_font.render('Game Over', True, WHITE)
+        gameover_rect = gameover.get_rect()
+        gameover_rect.topleft = (WINDOWWIDTH / 3 - 15, 125)
+        DISPLAYSURF.blit(gameover, gameover_rect)
+        paused = True
+        playagain = gameover_font.render('Press Q to quit', True, WHITE)
+        playagain_rect = playagain.get_rect()
+        playagain_rect.topleft = (WINDOWWIDTH / 4 - 40, 225)
+        DISPLAYSURF.blit(playagain, playagain_rect)
+        pygame.display.update()
 
 
 # artificialIntelligence for the computer
@@ -135,14 +159,14 @@ def artificialIntelligence(ball, XBALLSPEED, paddle2):
     elif XBALLSPEED > 0:
         if paddle2.centery < ball.centery:
             if YBALLSPEED == 0:
-                paddle2.y += 2
+                paddle2.y += 3
             else:
-                paddle2.y += 2  #YBALLSPEED
+                paddle2.y += 3  #YBALLSPEED
         else:
             if YBALLSPEED == 0:
-                paddle2.y -= 2
+                paddle2.y -= 3
             else:
-                paddle2.y -= 2  #YBALLSPEED
+                paddle2.y -= 3  #YBALLSPEED
     return paddle2
 
 
@@ -157,16 +181,42 @@ def displayScore(p1_score, p2_score):
     DISPLAYSURF.blit(player2, player2_rect)
 
 
+def spectator():
+    loop = True
+    while loop:
+        global ball, XBALLSPEED, YBALLSPEED, p1_score, p2_score
+        if ball.top <= 0 or ball.bottom >= WINDOWHEIGHT:
+            YBALLSPEED *= -1
+            wall_sound.play()
+        if ball.left <= 0 or ball.right >= WINDOWWIDTH:
+            XBALLSPEED *= -1
+            wall_sound.play()
+        ball = moveBall(ball, XBALLSPEED, YBALLSPEED)
+        drawArena()
+        drawBall(ball, WHITE)
+        displayScore(p1_score, p2_score)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                loop = False
+            elif event.type == MOUSEBUTTONDOWN:
+                loop = False
+
+
 def main():
     pygame.mixer.pre_init(44100, -16, 2, 4096)
     pygame.init()
 
     global DISPLAYSURF
-    global BASICFONT, BASICFONTSIZE
+    global BASICFONT, BASICFONTSIZE, gameover_font
     global regular_sound, faster_sound, fastest_sound, wall_sound, miss_sound
-    global ball
+    global ball, paused, p1_score, p2_score
     BASICFONTSIZE = 70
     BASICFONT = pygame.font.Font('visitor1.ttf', BASICFONTSIZE)
+    gameover_font = pygame.font.Font('visitor1.ttf', 50)
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     pygame.display.set_caption('Pong')
@@ -194,9 +244,14 @@ def main():
 
     # Draws the starting position of the arena
     drawArena()
+    drawBall(ball, WHITE)
+    spectator()
+    ball = pygame.Rect(ballX, ballY, 6, 6)
     drawPaddle(paddle1)
     drawPaddle(paddle2)
-    drawBall(ball, WHITE)
+
+    # Paused variable
+    paused = False
 
     pygame.mouse.set_visible(0)  # hide the cursor
     while True:
@@ -209,11 +264,21 @@ def main():
                 mousex, mousey = event.pos
                 paddle1.y = mousey
             elif event.type == KEYDOWN:
-                if event.key == K_SPACE:
-                    NUMHITS += 1
                 if event.key == K_q:
                     pygame.quit()
+                    sys.exit()
+                if event.key == K_p:
+                    paused = True
+                if event.key == K_y:
+                    paused = False
+                if event.key == K_n:
+                    pygame.quit()
+                    sys.exit()
 
+        game_over(p1_score, p2_score)
+
+        if paused:
+            continue
         drawArena()
         drawPaddle(paddle1)
         drawPaddle(paddle2)
